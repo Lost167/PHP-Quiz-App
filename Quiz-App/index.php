61064d88cd89d762d827801bee125d9486f184ad
Lost <90393645+Lost167@users.noreply.github.com>
2022-11-25 13:24:29 -0400

    Upload Base
    Quick upload of the base project

---
+++ Quiz-App/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TEMPORARY TITLE</title>
+        <link rel="icon" type="image/x-icon" href="images/logo/favicon.ico">
+        <link rel="stylesheet" href="main.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Hello World</h1>
+    </body>
+</html>
