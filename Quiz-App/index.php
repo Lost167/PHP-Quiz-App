70bbf9e255ef1ae6b56c2ec7fa375d7b56ef0221
Lost <90393645+Lost167@users.noreply.github.com>
2022-11-29 10:50:05 -0400

    Refresh base
    Make sure the "Ampps" folders are the same!

--- Quiz-App/index.php
+++ Quiz-App/index.php
@@ -8,6 +8,6 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Hello World I am an Alien</h1>
+        <h1>Hello World</h1>
     </body>
 </html>
