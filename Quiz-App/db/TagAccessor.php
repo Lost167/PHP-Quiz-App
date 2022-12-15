4ec0f2b22b8a270abd91a489bbe0372dc23d95ec
Bharatiben <bpatel06@mynbcc.ca>
2022-12-14 21:39:33 -0400

    Update TagAccessor.php
    done

--- Quiz-App/db/TagAccessor.php
+++ Quiz-App/db/TagAccessor.php
@@ -1,20 +1,32 @@
 <?php
 
-require_once('dbconnect.php');
+//require_once('dbconnect.php');
+require_once 'ConnectionManager.php';
+require_once('QuestionTagAccessor.php');
+require_once('QuestionAccessor.php');
 require_once(__DIR__ . '/../entity/Tag.php');
+require_once(__DIR__ . '/../entity/QuestionTag.php');
+require_once(__DIR__ . '/../entity/Question.php');
+require_once (__DIR__ .  '/../utils/ChromePhp.php');
 
 class TagAccessor {
 
     public function getAllTags() {
         $results = [];
+        $cm = new ConnectionManager();
+
         try {
-            $conn = connect_db();
-            $stmt = $conn->prepare("select * from Tag");
+            $conn = $cm->connect_db();
+            $stmt = $conn->prepare("select * from tag");
             $stmt->execute();
             $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             foreach ($dbresults as $r) {
-                $obj = new Tag($r["tagID"], $r["tagName"], $r["tagCategoryName"]);
+                $tagID = intval($r["tagID"]);
+                $tagName = $r['tagName'];
+                $tagCategory = $r['tagCategoryName'];
+                
+                $obj = new Tag($tagID, $tagName, $tagCategory);
                 array_push($results, $obj);
             }
         } catch (Exception $e) {
@@ -24,19 +36,23 @@ class TagAccessor {
                 $stmt->closeCursor();
             }
         }
+
         return $results;
     }
-
+    
     public function getTagsForQuestion($questionID) {
         $results = [];
+        $cm = new ConnectionManager();
         try {
-            $conn = connect_db();
+            $conn = $cm->connect_db();
             $stmt = $conn->prepare("select * from QuestionTag join Tag using(tagID) where questionID = :questionID");
             $stmt->bindParam(":questionID", $questionID);
             $stmt->execute();
             $dbresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             foreach ($dbresults as $r) {
+                //$obj = new QuestionTag($r["questionID"], intval($r["tagID"]));
+                //$obj = new Tag(intval($r["tagID"]), $r["tagName"], $r["tagCategoryName"]);
                 $obj = new Tag($r["tagID"], $r["tagName"], $r["tagCategoryName"]);
                 array_push($results, $obj);
             }
@@ -49,5 +65,7 @@ class TagAccessor {
         }
         return $results;
     }
+    
+    
 
 }
